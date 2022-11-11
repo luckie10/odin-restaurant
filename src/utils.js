8185580c8426188cb405a3e124def3256f350a26
Boban Kilvin <btrajcevski10@gmail.com>
2022-11-11 12:00:41 -0500

    Rename key to name, add annotations

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,16 @@
+/**
+ * Create an HTML element with any attribute
+ *
+ * @param {string} tagName - Name of HTML tag
+ * @param {{name: string, value: string}} attributes - Object of attributes to be appened to HTML element
+ * @returns {Element}
+ */
 function createElement(tagName, attributes) {
   const element = document.createElement(tagName);
 
   if (attributes) {
-    Object.entries(attributes).map(([key, value]) => {
-      element.setAttribute(key, value);
+    Object.entries(attributes).map(([name, value]) => {
+      element.setAttribute(name, value);
     });
   }
 
