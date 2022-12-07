3bc2a34986255b18fc8d7772c59e9cc1af364f6a
Boban Kilvin <btrajcevski10@gmail.com>
2022-12-07 04:14:36 -0500

    Added icons to assets

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import facebook from "./icons/facebook.svg";
+import instagram from "./icons/instagram.svg";
+import twitter from "./icons/twitter.svg";
+
+export const icons = {
+  facebook,
+  instagram,
+  twitter,
+};
